f7182e6ec68fde07a07f58fb27e74ebb08770889
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2024-05-05 10:33:53 +0300

    Добавьте файлы проекта.

---
+++ charge.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "directory.h"
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	directory dgfff("s4e45d", "ff523nms", "eg53d", "gr14s", "3e00qd");
+	directory dfbdf("1", "2", "3", "4", "5");
+	directory drghf("5", "7", "8", "9", "10");
+	directory::show();
+	return 0;
+}
